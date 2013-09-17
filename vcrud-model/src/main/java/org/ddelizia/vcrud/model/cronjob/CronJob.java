b8022f98c21d0d5f36072d1ea18669a5f6b0f67f
danilo.delizia <danilo.delizia@oramon.nl>
2013-09-17 18:18:28 +0200

    CronJob Model

---
+++ vcrud-model/src/main/java/org/ddelizia/vcrud/model/cronjob/CronJob.java
@@ -0,0 +1,18 @@
+package org.ddelizia.vcrud.model.cronjob;
+
+import org.ddelizia.vcrud.model.system.VcrudModel;
+
+import javax.persistence.Entity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: danilo.delizia
+ * Date: 17/09/13
+ * Time: 9:44
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+public class CronJob extends VcrudModel{
+
+
+}
